01942b5f142295dbf96979666a96b6500e44f33b
aelskens <arthurelsk@gmail.com>
2025-07-31 14:47:05 +0000

    chore: add import in main __init__.py for package purposes

--- src/omniglue/__init__.py
+++ src/omniglue/__init__.py
@@ -4,3 +4,6 @@ try:
     __version__ = version("omniglue")
 except PackageNotFoundError:
     __version__ = "dev"
+
+from dense_feature_extraction import DenseExtract
+from model import FlexibleOmniGlue
